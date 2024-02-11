5692451959e7b78ca9da636ad8b5c99c084e7b9e
brian d foy <briandfoy@pobox.com>
2024-02-11 15:38:45 -0500

    Update email addresses

--- lib/Distribution/Cooker.pm
+++ lib/Distribution/Cooker.pm
@@ -618,11 +618,11 @@ This module is in Github:
 
 =head1 AUTHOR
 
-brian d foy, C<< <bdfoy@cpan.org> >>
+brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2008-2021, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2008-2021, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 You may redistribute this under the same terms as Perl itself.
 
